ea1c5539944ee21918e3a95c75d24270e0e4c1dc
rudnic <werdont@gmail.com>
2023-01-16 00:06:26 +0300

    add new test

--- calc.js
+++ calc.js
@@ -10,6 +10,10 @@ function calculate(arg1, arg2, operation) {
     if (operation == "-") {
         return arg1 - arg2;
     }
+
+    if (operation == "*") {
+        return arg1 * arg2;
+    }
 }
 
 module.exports.calculate = calculate;
